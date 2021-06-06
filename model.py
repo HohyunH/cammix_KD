6822492013ad7e6ca9eabf49833041bfabaae046
HohyunH <46701548+HohyunH@users.noreply.github.com>
2021-06-06 22:28:37 +0900

    Add files via upload
    model about vanilla cnn & channel reduction resnet18

---
+++ model.py
@@ -0,0 +1,148 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BasicBlock(nn.Module):
+    expansion = 1
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+
+        self.residual_function = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels * BasicBlock.expansion, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels * BasicBlock.expansion),
+        )
+
+        self.shortcut = nn.Sequential()
+
+        self.relu = nn.ReLU()
+
+        if stride != 1 or in_channels != BasicBlock.expansion * out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * BasicBlock.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels * BasicBlock.expansion)
+            )
+
+    def forward(self, x):
+        x = self.residual_function(x) + self.shortcut(x)
+        x = self.relu(x)
+        return x
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_block, num_classes=6, init_weights=True):
+        super().__init__()
+
+        self.in_channels=64
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.conv2_x = self._make_layer(block, 32, num_block[0], 1)
+        self.conv3_x = self._make_layer(block, 64, num_block[1], 2)
+        self.conv4_x = self._make_layer(block, 128, num_block[2], 2)
+        self.conv5_x = self._make_layer(block, 256, num_block[3], 2)
+
+        self.avg_pool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Linear(256 * block.expansion, num_classes)
+
+        # weights inittialization
+        if init_weights:
+            self._initialize_weights()
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self,x):
+        output = self.conv1(x)
+        output = self.conv2_x(output)
+        x = self.conv3_x(output)
+        x = self.conv4_x(x)
+        x = self.conv5_x(x)
+        x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+    # define weight initialization function
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+
+        self.conv = nn.Sequential(
+
+            nn.Conv2d(3, 32, 3),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, 3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, 3),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(128, 128, 3),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+        )
+
+        self.fc1 = nn.Linear(128 * 5 * 5, 512)
+        self.fc2 = nn.Linear(512, 6)
+
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(x.shape[0], -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+if __name__ == "__main__":
+
+    def small_resnet18():
+        return ResNet(BasicBlock, [2, 2, 2, 2])
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = small_resnet18().to(device)
+    x = torch.randn(3, 3, 224, 224).to(device)
+    output = model(x)
+    print(output.size())
+
+
+    cnn_model = SimpleCNN().to(device)
+    x = torch.randn(3, 3, 224, 224).to(device)
+    output = model(x)
+    print(output.size())
+
+
